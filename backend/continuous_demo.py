9e5dcaba590fef04d70cac9112f439d1b8332d95
apend10 <apendela10@gmail.com>
2025-01-26 10:38:41 -0600

    flaskized the backend

--- backend/continuous_demo.py
+++ backend/continuous_demo.py
@@ -2,17 +2,16 @@ from video_stream import prompting
 from video_stream import main
 from multimodal import describe
 import time
-import os
 
-print("BEFORE WHILE LOOP")
-start = 0
+print("----------------------------------------")
 while(True):
+    res = []
     n = main.main()
-    print("main.main EXECUTED")
     print(f"WE HAVE DETECTED {n} INDIVIDUALS.")
     for i in range(n):
         img_path = f"frontend/public/images/person_{i}.jpg"
-        print(img_path)
-        res = describe(img_path)#prompting.describe_image(img_path)
+        res.append(describe(img_path))
         print(res)
+
     time.sleep(10)
+print("----------------------------------------")
